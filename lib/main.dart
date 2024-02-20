e32d3faa396a6e90f32f3771927f42b8f79b79c1
Yuvaraaj17 <yuvaraaj5910@gmail.com>
2024-02-20 21:59:42 +0530

    removed drawer

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,6 @@ class _AppState extends State<App> {
                     ),
                   ],
                 ),
-                drawer: const NavDrawer(),
               )
           )
       ),
